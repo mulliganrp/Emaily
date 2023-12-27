f16b444f57afaa2f5aa30cb3e968726fc85e3e82
Ryan Mulligan <ryanmulligan@emilys-air.lan>
2023-12-27 10:59:52 -0500

    Removing unnecessary keys

--- FullstackWorkspace/prod/server/config/keys.js
+++ FullstackWorkspace/prod/server/config/keys.js
@@ -4,8 +4,3 @@ if (process.env.NODE_ENV === 'production') {
 } else {
     module.exports = require('./dev');
 }
-
-
-googleClientID: '166645623056-paqnot043ktj6f6d2339jhlq562e0256.apps.googleusercontent.com',
-googleClientSecret: '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',
-mongoURI: 'mongodb+srv://mulliganrp:2PRWiXN8qRtpTmiz@cluster0.4wlroue.mongodb.net/emailyprod?retryWrites=true&w=majority'
